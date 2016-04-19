06871dfde4da0809cf515d619f0bfad230d7087f
Bailey Hollingsworth <bailey.a.hollingsworth-1@ou.edu>
2016-04-18 22:13:49 -0600

    1

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ function postMessage() {
 
   body = {
     "bot_id" : botID,
-    "text" : botResponse
+    "text" : "I love Bailey and Chase"
   };
 
   console.log('sending ' + botResponse + ' to ' + botID);
